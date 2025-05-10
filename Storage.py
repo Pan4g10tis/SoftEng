d06225a67c317afb812bb16b2eed1420a6cd7061
Panagiotis Psaltiras <147633002+Pan4g10tis@users.noreply.github.com>
2025-05-10 17:30:49 +0300

    Create Storage.py

---
+++ Storage.py
@@ -0,0 +1,6 @@
+class Storage:
+   def __init__(self, storage_id, address, email, phone):
+       self.id = storage_id
+       self.address = address
+       self.email = email
+       self.phone = phone
