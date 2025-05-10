48c7a527481c820b30d1feb86536763aceee53a2
Panagiotis Psaltiras <147633002+Pan4g10tis@users.noreply.github.com>
2025-05-10 19:04:31 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,3 +4,13 @@ class DataBase:
        self.password = password
        self.host = host
        self.database = database
+
+   def connect_db(self):
+       return mysql.connector.connect(
+           user=self.user,
+           password=self.password,
+           host=self.host,
+           database=self.database
+       )
+
+
