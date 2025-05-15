451eddc79da637d0391ec32e84be02eaef39093c
GeoZann <142328756+GeoZann@users.noreply.github.com>
2025-05-15 22:24:08 +0300

    Update Store.py

--- Store.py
+++ Store.py
@@ -4,3 +4,24 @@ class Store:
        self.address = address
        self.email = email
        self.phone = phone
+
+
+   def show_orders(self, mydb):
+       conn = mydb.connect_db()
+       cursor = conn.cursor()
+       cursor.execute("SELECT id, priority, status, start_date, end_date FROM store_order WHERE store = %s",
+                      (self.id,))
+       rows = cursor.fetchall()
+       orders = []
+       for row in rows:
+           order = StoreOrder(row[0], self.id, row[1], row[2], row[3], row[4], [], [])
+           cursor.execute("SELECT product,amount FROM store_order_product WHERE order_num = %s",
+                          (order.id,))
+           prod_rows = cursor.fetchall()
+           for prod_row in prod_rows:
+               order.products.append(prod_row[0])
+               order.amounts.append(prod_row[1])
+           orders.append(order)
+       conn.close()
+       return orders
+
