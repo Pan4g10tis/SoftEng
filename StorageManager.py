de0ca76a6b25dc20c48e55c0c7794e7e49e4d730
Panagiotis Psaltiras <147633002+Pan4g10tis@users.noreply.github.com>
2025-05-16 21:47:41 +0300

    Update StorageManager.py

--- StorageManager.py
+++ StorageManager.py
@@ -21,3 +21,14 @@ class StorageManager:
        conn.close()
        my_storage = Storage(*result)
        return my_storage
+
+   @classmethod
+   def create(cls, mydb, username):
+       conn = mydb.connect_db()
+       cursor = conn.cursor()
+       cursor.execute("SELECT manager.username, password, name, surname, email, phone, storage "
+                      "FROM manager INNER JOIN storage_manager On manager.username = storage_manager.username "
+                      "WHERE manager.username = %s", (username,))
+       result = cursor.fetchone()
+       manager = StorageManager(*result)
+       return manager
